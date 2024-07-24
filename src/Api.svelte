22e5e7b1d95d53b6973128a690392a1370217ec0
ChristopherTsh <156902121+ChristopherTsh@users.noreply.github.com>
2024-07-24 15:28:26 +0200

    "Remove existing App component implementation, replace with new Api import and minimal UI structure"

---
+++ src/Api.svelte
@@ -0,0 +1,22 @@
+<script>
+    let products = []
+fetch("https://fakestoreapi.com/products")
+.then(response => response.json())
+.then(data => {
+products = data;
+    console.log(data);
+}) 
+
+</script>
+
+
+    <div class="container">
+        {#each products as product (product.id)}
+       <h1>{product.title}</h1>
+       <p>{product.price}</p>
+       {/each}
+</div>
+
+<style>
+
+</style>
