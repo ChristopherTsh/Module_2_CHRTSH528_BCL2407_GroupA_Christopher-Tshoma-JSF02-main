a9cc847036d4fd7404f85835f0906ac6c2d5008a
ChristopherTsh <156902121+ChristopherTsh@users.noreply.github.com>
2024-07-25 16:46:02 +0200

    created a routes folder created Productdetails

--- src/routes/App.svelte
+++ src/routes/App.svelte
@@ -11,11 +11,11 @@
   </Router>
 
 <style>
-    .home {
+    /* .home {
         font-family: Arial, sans-serif;
         padding: 2rem;
         background-color: #f5f5f5;
-    }
+    } */
 </style>
 
 
---
+++ src/routes/ProductDetails.svelte
@@ -0,0 +1,39 @@
+<script>
+  import { onMount } from "svelte";
+  // import { params } from 'svelte-routing';
+
+  let product = null;
+  // let productId = params.id;
+
+  onMount(async () => {
+    try {
+      const response = await fetch(`https://fakestoreapi.com/products/`);
+      const data = await response.json();
+      product = data;
+    } catch (error) {
+      console.error(error);
+    }
+  });
+</script>
+
+{#if product}
+<div class="product-detail">
+  <img class="images" src={product.image} alt="">
+  <h1>{product.title}</h1>
+  <p>{product.price}</p>
+  <p>{product.description}</p>
+</div>
+{/if}
+
+<style>
+  .product-detail {
+    max-width: 600px;
+    margin: 0 auto;
+    padding: 2rem;
+    text-align: center;
+  }
+  .images {
+    width: 300px;
+    height: 300px;
+  }
+</style>
