d349bd5c397251486c30e5f7b3849491837b7c2d
ChristopherTsh <156902121+ChristopherTsh@users.noreply.github.com>
2024-07-25 11:25:20 +0200

    "Added Modal component and button to toggle it, updated App.svelte structure"

--- src/App.svelte
+++ src/App.svelte
@@ -1,4 +1,4 @@
-<script>
+<!-- <script>
 	import Api from './Api.svelte'
 	
 </script>
@@ -7,4 +7,40 @@
 
 <main>
     <Api />
+</main> -->
+
+<script>
+	import Api from './Api.svelte'
+	import Modal from './Modal.svelte';
+
+	let showModal = false;
+</script>
+
+<main>
+    <Api />
 </main>
+<button on:click={() => (showModal = true)}> show modal </button>
+
+<Modal bind:showModal>
+	<h2 slot="header">
+		modal
+		<small><em>adjective</em> mod·al \ˈmō-dəl\</small>
+	</h2>
+
+	<ol class="definition-list">
+		<li>of or relating to modality in logic</li>
+		<li>
+			containing provisions as to the mode of procedure or the manner of taking effect —used of a
+			contract or legacy
+		</li>
+		<li>of or relating to a musical mode</li>
+		<li>of or relating to structure as opposed to substance</li>
+		<li>
+			of, relating to, or constituting a grammatical form or category characteristically indicating
+			predication
+		</li>
+		<li>of or relating to a statistical mode</li>
+	</ol>
+
+	<a href="https://www.merriam-webster.com/dictionary/modal">merriam-webster.com</a>
+</Modal>
