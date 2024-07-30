885d3b7d131b7461ff3dee45ac3f7f9d3326aa63
ChristopherTsh <156902121+ChristopherTsh@users.noreply.github.com>
2024-07-30 14:23:26 +0200

    "Added loader animation to Home and ProductDetails routes"

--- src/routes/+Home.svelte
+++ src/routes/+Home.svelte
@@ -163,7 +163,12 @@
 
 <div class="container">
   {#if loading}
-    <p>Loading...</p>
+  <div class="loader">
+    <svg viewBox="0 0 50 50">
+      <circle class="ring" cx="25" cy="25" r="20"></circle>
+      <circle class="ball" cx="25" cy="5" r="3.5"></circle>
+    </svg>
+  </div>
   {:else}
     {#each filteredProducts as product (product.id)}
       <div class="product">
